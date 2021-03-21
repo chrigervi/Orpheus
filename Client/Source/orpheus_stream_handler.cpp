9d1a4e10610ec75046e3040458520a2890bff703
chrigervi <80692663+chrigervi@users.noreply.github.com>
2021-03-21 15:39:52 +0100

    Update orpheus_stream_handler.cpp

--- Client/Source/orpheus_stream_handler.cpp
+++ Client/Source/orpheus_stream_handler.cpp
@@ -1,4 +1,5 @@
 /* Copyright (c) 2021, Christian Hertel */
+// TODO: Add ScopedLock to maintain thread-security
 
 #include "orpheus_stream_handler.h"
 
@@ -8,29 +9,29 @@ orpheus::AudioStreamHandler::~AudioStreamHandler()
 }
 void orpheus::AudioStreamHandler::init(float sample_rate)
 {
-	//ORPH_globallock();
 	ORPHLOG("Initialising . . .");
 
 	if (!active) {
 		this->sample_rate = sample_rate;
-		//active = true;
-		//return _init();
 		sendActionMessage("STREAMHANDLER_INIT");
+		
+		// _init() does probably need long to return because of network communication and such.
+		// In order to not block the JUCE message thread the function gets called on a seperate thread.
 		juce::Thread::launch([this] { 
-			if (_init()) sendActionMessage("STREAMHANDLER_INIT_SUCCESS");
-			else sendActionMessage("STREAMHANDLER_INIT_FAIL");
+			if (_init()) 
+				sendActionMessage("STREAMHANDLER_INIT_SUCCESS");
+			else 
+				sendActionMessage("STREAMHANDLER_INIT_FAIL");
 			active = true; 
 		});
 	}
 }
 void orpheus::AudioStreamHandler::update(juce::AudioBuffer<float>& buffer, juce::AudioBuffer<float>& host_out, juce::AudioBuffer<float>& guest_out)
 {
-	//ORPH_globallock();
 	if (isActive()) _update(buffer, host_out, guest_out);
 }
 void orpheus::AudioStreamHandler::stop()
 {
-	//ORPH_globallock();
 	ORPHLOG("Stopping . . . ");
 
 	if (active) _stop();
